a5d509db319598725716179c9601323e82041f28
KimJeeHyung <31695396+KimJeeHyung@users.noreply.github.com>
2018-06-18 17:29:52 +0900

    체력 스크립트
    PlayerMove는 준영이형의 스크립트에 공격 코드만 추가
    각 파일 안에 주석으로 설명을 써놓았습니다.
    PlayerMove는 플레이어에, Enemy는 적 캐릭터에, HP와 HP_Enemy는 각각 플레이어와 적 캐릭터의 체력 텍스트에 적용하면 됩니다.

---
+++ Script/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if (coll.transform.tag == "Player") {   //플레이어와 충돌하면
+			HP.instance.hpControl (20);         //플레이어의 체력을 20 깎음
+			Debug.Log ("충돌");
+		}
+	}
+}
---
+++ Script/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	private float moveSpeed = 5.0f;    //이동 속도
+	private float jumpPower = 10.0f;    //점프 속도
+	private bool isGrounded = true;
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow))   //입력키가 위화살표면 실행함
+		{
+			Jump();
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))    //왼쪽화살표 입력시 실행함
+		{
+			Vector3 scale = transform.localScale;
+			scale.x = -Mathf.Abs(scale.x);
+			transform.localScale = scale;
+			transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetKey(KeyCode.RightArrow))    //오른쪽화살표 입력시 실행함
+		{
+			Vector3 scale = transform.localScale;
+			scale.x = +Mathf.Abs(scale.x);
+			transform.localScale = scale;
+			transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+		}
+		if(Input.GetKeyDown(KeyCode.Z))         //Z키 입력시 실행함
+		{
+			HP_Enemy.instance.hpControl (25);   //적의 체력을 25 깎음
+			Debug.Log ("공격");
+		}
+	}
+
+	void Jump()
+	{
+		if (!isGrounded || rb.velocity.y != 0) { return; }
+
+		rb.AddForce(new Vector2(0, 1) * jumpPower, ForceMode2D.Impulse);
+		isGrounded = false;
+	}
+
+	private void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Platform")
+		{
+			isGrounded = true;
+		}
+	}
+}
